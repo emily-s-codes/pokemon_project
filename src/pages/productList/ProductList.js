bdb2d3c8a917788802f3f060f05417d96364fa52
emilysupercode <sheil.emily.sc@gmail.com>
2022-12-06 09:57:35 +0100

    detail list with nav

--- src/pages/productList/ProductList.js
+++ src/pages/productList/ProductList.js
@@ -1,8 +1,10 @@
+import Navigation from "../../components/navigation/Navigation";
 import ProductDetail from "../productDetail/ProductDetail";
 import "./ProductList.css";
 
 const ProductList = () => {
     return (<section>
+        <Navigation />
         <p>List of Products</p>
         <ProductDetail />
         <ProductDetail />
